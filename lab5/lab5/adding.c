c295ec87d7b51b68c014ba324023e63dc30b55ce
danil200subbotin <danil200subbotin@gmail.com>
2020-05-05 01:28:20 +0500

    start workin with adding

--- lab5/lab5/adding.c
+++ lab5/lab5/adding.c
@@ -7,4 +7,23 @@
 //
 
 #include "adding.h"
+#include "strukts.h"
+#include "readln.h"
 
+int adding(Graph *graph) {
+    printf("сейчас в графе %d узлов\n", graph->n);
+    switch (graph->n) {
+    case 0:
+            printf("Поздравляем, вы намереваетесь ввести свой первый элемент!\n");
+            printf("введите ключ 1 (ось X)----->");
+            scanf("%d", graph->root->leaves[0]->x);
+            printf("введите ключ 2 (ось Y)----->");
+            scanf("%d", graph->root->leaves[0]->y);
+            printf("введите информацию для элемента----->");
+            graph->root->leaves[0]->info = readln();
+      break;
+    default:
+      /*Код, который выполнится, если ниодно из константых значению не соответствует значение в переменной variable*/
+      break;
+    }
+};
