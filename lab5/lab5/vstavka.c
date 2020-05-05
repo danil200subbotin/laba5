ee8f5205fdef334485a638d39175dd86c1bc05c3
danil200subbotin <danil200subbotin@gmail.com>
2020-05-05 12:01:38 +0500

    doing vstavku

--- lab5/lab5/vstavka.c
+++ lab5/lab5/vstavka.c
@@ -8,9 +8,27 @@
 
 #include "vstavka.h"
 #include "strukts.h"
+#include <stdlib.h>
 
 int vstavka(int x, int y, Node* ukazka) {
-    if (ukazka->leaves[N-1] != NULL)
+    if (ukazka->leaves[N-1] != NULL) {
+        List* list = NULL;
+        List* new = (List *)calloc(1, sizeof(List));
+        new->x = x;
+        new->y = y;
+        for (int i = 0; i < N; ++i) {
+            if ((x == ukazka->leaves[i]->x) & (y == ukazka->leaves[i]->y)) {
+                printf("Запрещаю данное сочетание ключей!!!!!!\n");
+                free(new);
+                return -1;
+            }
+            if (x < ukazka->leaves[i]->x) {
+                list = ukazka->leaves[i];
+                ukazka->leaves[i] = new;
+                
+            }
+        }
+     }
     
     
 }
