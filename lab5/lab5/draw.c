fbccf1cf4cc77f8dc7faf52e1c10b57ac093c78a
danil200subbotin <danil200subbotin@gmail.com>
2020-05-09 02:49:12 +0500

    подключил SDL

---
+++ lab5/lab5/draw.c
@@ -0,0 +1,9 @@
+//
+//  draw.c
+//  lab5
+//
+//  Created by Данил Морозов on 09/05/2020.
+//  Copyright © 2020 Данил Морозов. All rights reserved.
+//
+
+#include "draw.h"
---
+++ lab5/lab5/draw.h
@@ -0,0 +1,14 @@
+//
+//  draw.h
+//  lab5
+//
+//  Created by Данил Морозов on 09/05/2020.
+//  Copyright © 2020 Данил Морозов. All rights reserved.
+//
+
+#ifndef draw_h
+#define draw_h
+
+#include <stdio.h>
+
+#endif /* draw_h */
